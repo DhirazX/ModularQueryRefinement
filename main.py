bfc17b172bf52c8baa808043d6162f6c0e87f8e5
Dhiraj <dhrjpoudyal@gmail.com>
2025-05-30 20:19:35 +0545

    Basic preprocessing setup

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from nlp_utils.preprocessor import preprocess
+
+# Read data from "data.txt"
+with open("data.txt" , "r", encoding="utf-8") as file:
+    text = file.read()
+
+# Preprocess text
+preprocessed_text = preprocess(text)
+
+# Print the result
+print("Preprocessed Text: ", preprocessed_text)
---
+++ nlp_utils/preprocessor.py
@@ -0,0 +1,17 @@
+import nltk
+import re
+
+def preprocess(text: str):
+    # Converting text to lower case
+    text = text.lower()
+
+    # Removing Punctuations
+    text = re.sub(r'[^\w\s]', '', text)
+
+    #Removing numbers
+    text = re.sub(r'\d+', '', text)
+
+    #Removing Whitespace
+    text = text.strip()
+
+    return text
